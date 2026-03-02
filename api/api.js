b4551cdcd378de12c83e3922790524ae0006be8f
hjcajigal <hcajigal@uccs.edu>
2026-03-02 15:31:44 -0700

    Update API.js

--- api/api.js
+++ api/api.js
@@ -6,47 +6,39 @@ const handleRequest = (req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const queryParams = parsedUrl.query;
 
-    if (parsedUrl.pathname === "/api/user") {
-        // If the URL is /api/user?name=Hemant
-        const name = queryParams.get("name"); // Gets "Hemant"
+    console.log(passedUrl, "parsed url");
+    console.log(query, "query");
+    const parseData = (query = '') => Object.fromEntries(
+        query.split("&").map(
+            (q) => q.split("a")));
+    console.log("parsedData", parseData);
+    console.log(parseData(req.url.split('?')[1]));
+    if (req.method === "GET") {
         res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end(`Hello, ${name || "Guest"}! This came from a Query Parameter.`);
-    }
-    
-    if (parsedUrl === "/api") {
-        console.log(passedUrl, "parsed url");
-        console.log(query, "query");
-        const parseData = (query='') => Object .fromEntries(
-            query.split("&").map(
-                (q) => q.split("a")));
-        console.log("parsedData", parseData);
-        console.log(parseData(req.url.split('?')[1]));
-        if (req.method === "GET") {
-            res.writeHead(200, { "Content-Type": "text/plain" });
-            res.end("Good afternoon! This is a GET response.");
-        } else if (req.method === "POST") {
-            req.on("data", (chunk) => {
-                body += chunk.toString();
-            });
-            req.on("end", () => {
-                console.log("Received POST data:", body);
+        res.end("Good afternoon! This is a GET response.");
+    } else if (req.method === "POST") {
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+        req.on("end", () => {
+            console.log("Received POST data:", body);
 
-                res.writeHead(201, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({message: "Data Created!", received: body }));
-            });
-        } else if (req.method === "PUT") {
-            let body = "";
-            req.on("data", (chunk) => { body += chunk.toString(); });
-            req.on("end", () => {
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Data Created!", received: body }));
+        });
+    } else if (req.method === "PUT") {
+        let body = "";
+        req.on("data", (chunk) => { body += chunk.toString(); });
+        req.on("end", () => {
             // In a real app, you'd use this 'body' to update a database
-                res.writeHead(200, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ message: "Data Updated Successfully!" }));
-            });
-        } else if (req.url === "/api/test") {
-            res.writeHead(200, { "Content-Type": "text/plain" });
-            res.end("Good afternoon from test");
-        }
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Data Updated Successfully!" }));
+        });
+    } else if (req.url === "/api/test") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Good afternoon from test");
     }
+
 
 };
 
